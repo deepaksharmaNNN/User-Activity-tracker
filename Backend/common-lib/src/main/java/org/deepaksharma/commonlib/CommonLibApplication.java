d213956ad5afc93b12389d570d39fd1b4892a835
Deepak Sharma <dsharma2828@gmail.com>
2025-03-23 09:30:05 +0530

    INIT

---
+++ Backend/common-lib/src/main/java/org/deepaksharma/commonlib/CommonLibApplication.java
@@ -0,0 +1,13 @@
+package org.deepaksharma.commonlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonLibApplication.class, args);
+    }
+
+}
