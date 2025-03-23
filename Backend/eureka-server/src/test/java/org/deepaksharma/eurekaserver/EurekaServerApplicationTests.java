b14e7718f0c69e20cbda381e102641755c7477dc
Deepak Sharma <dsharma2828@gmail.com>
2025-03-23 22:08:29 +0530

    Added Eureka sever Module

---
+++ Backend/eureka-server/src/test/java/org/deepaksharma/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deepaksharma.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
